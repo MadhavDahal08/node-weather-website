4b1219ce2cb94b671d1678844d61f4c833d3cc0a
Madhav <madhavdahal4@gmail.com>
2020-07-17 10:21:26 +0545

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Hello form the client site javscript');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
